8aed1d5d019e078e8d64dfe3192ec732ea0ec6f0
Activity_4 <egyputra78@gmail.com>
2023-04-02 05:13:10 +0700

    membuat form register untuk Nama

--- lib/register.dart
+++ lib/register.dart
@@ -17,20 +17,15 @@ class _registerState extends State<register> {
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-        floatingActionButton: SizedBox(       
-          height:50,
-          width:50,
-          child:FloatingActionButton(        
-              child: Icon(Icons.arrow_back_sharp), //child widget inside this button
-              shape: BeveledRectangleBorder(
-                borderRadius: BorderRadius.circular(5)
-              
-              ),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => login()));
-                //task to execute when this button is pressed
-              },
-            ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: FloatingActionButton(
+            child: Icon(Icons.arrow_back_sharp),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => login()));
+            },
+          ),
         ),
         body: SingleChildScrollView(
             child: Padding(
@@ -39,11 +34,55 @@ class _registerState extends State<register> {
               child: Column(
             children: [
               Container(
+                  margin: EdgeInsets.only(top: 12),
+                  alignment: Alignment.topCenter,
+                  child: CircleAvatar(
+                    backgroundImage:
+                        const AssetImage('assets/image/pokemon_logo.png'),
+                    radius: 60,
+                  )),
+              Container(
                 margin: EdgeInsets.only(top: 12),
                 alignment: Alignment.topCenter,
-                  child: Image.asset('assets/image/pokemon_logo.png')
-
-                  )
+                child: RichText(
+                  text: const TextSpan(
+                      text: "Register",
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.yellow)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20),
+                alignment: Alignment.centerLeft,
+                child: RichText(
+                    text: TextSpan(
+                        text: "Nama",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold))),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 15),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                          keyboardType: TextInputType.name,
+                          controller: namaController,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.person),
+                          ),
+                          validator: (value) {
+                            if (value == '') {
+                              return 'Please Enter Your Name';
+                            }
+                          })
+                    ],
+                  )),
             ],
           )),
         )));
